9c44d213c1eb5620590b574ac9bc1f2645fb7ee4
Phạm Minh Anh <phamminhanh@MacBook-Air-cua-Pham.local>
2026-07-29 11:32:04 +0700

    change to class

--- script.js
+++ script.js
@@ -1,97 +1,97 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-    if (!new.target) {
-        throw Error("You must use the 'new' operator to call the constructor")
-    }
-    this.id = crypto.randomUUID();
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
-
-Book.prototype.info = function() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "read" : "not read yet"}`
-};
-
-Book.prototype.toggleRead = function() {
-    this.read = !this.read;
-}
-
-function addBookToLibrary(title, author, pages, read) {
-    const newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
-}
-
-function displayBooks() {
-    const table = document.querySelector("table");
-    for (let book of myLibrary) {
-        const row = document.createElement("tr");
-        for (let property in book) {
-            if (typeof book[property] === "function" || property === "id") {}
-            else if (property ==="read") {
-                const readButton = document.createElement("button")
-                readButton.className = "read";
-                if(book.read) {
-                    readButton.textContent = "read";
-                } else {
-                    readButton.textContent = "notread";
-                }
-                readButton.setAttribute("data-index-number", book.id)
-                row.appendChild(readButton)
-
-                readButton.addEventListener("click", function(event) {
-                    const target = event.target;
-                    const id = target.dataset.indexNumber;
-                    const targetBook = myLibrary.find(book => book.id === id);
-                    targetBook.toggleRead();
-
-                    clearTable();
-                displayBooks();
-
-                })
-            }
-            else {
-                const cell = document.createElement("td");
-                cell.textContent = book[property];
-                row.appendChild(cell);
-            }
+class Book {
+    constructor(title, author, pages, read) {
+        if (!new.target) {
+            throw Error("You must use the 'new' operator to call the constructor")
         }
+        this.id = crypto.randomUUID();
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 
-        const removeButton = document.createElement("button")
-        removeButton.className = "remove";
-        removeButton.textContent = "Remove";
-        removeButton.setAttribute("data-index-number", book.id)
-        row.appendChild(removeButton)
+    info() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "read" : "not read yet"}`;
+    }
 
-        removeButton.addEventListener("click", function(event) {
-            const target = event.target;
-            const id = target.dataset.indexNumber;
-            const index = myLibrary.findIndex(book => book.id === id);
-            
-            myLibrary.splice(index, 1);
+    toggleRead() {this.read = !this.read;}
 
-            clearTable();
-            displayBooks();
-        })
+    static addBookToLibrary (title, author, pages, read) {
+        const newBook = new Book(title, author, pages, read);
+        myLibrary.push(newBook);
+    }
 
-        table.appendChild(row);
+    static displayBooks () {
+        const table = document.querySelector("table");
+        for (let book of myLibrary) {
+            const row = document.createElement("tr");
+            for (let property in book) {
+                if (typeof book[property] === "function" || property === "id") {}
+                else if (property ==="read") {
+                    const readButton = document.createElement("button")
+                    readButton.className = "read";
+                    if(book.read) {
+                        readButton.textContent = "read";
+                    } else {
+                        readButton.textContent = "notread";
+                    }
+                    readButton.setAttribute("data-index-number", book.id)
+                    row.appendChild(readButton)
+
+                    readButton.addEventListener("click", function(event) {
+                        const target = event.target;
+                        const id = target.dataset.indexNumber;
+                        const targetBook = myLibrary.find(book => book.id === id);
+                        targetBook.toggleRead();
+
+                        Book.clearTable();
+                        Book.displayBooks();
+
+                    })
+                }
+                else {
+                    const cell = document.createElement("td");
+                    cell.textContent = book[property];
+                    row.appendChild(cell);
+                }
+            }
+
+            const removeButton = document.createElement("button")
+            removeButton.className = "remove";
+            removeButton.textContent = "Remove";
+            removeButton.setAttribute("data-index-number", book.id)
+            row.appendChild(removeButton)
+
+            removeButton.addEventListener("click", function(event) {
+                const target = event.target;
+                const id = target.dataset.indexNumber;
+                const index = myLibrary.findIndex(book => book.id === id);
+                
+                myLibrary.splice(index, 1);
+
+                Book.clearTable();
+                Book.displayBooks();
+            })
+
+            table.appendChild(row);
+        }
+    }
+
+    static clearTable() {
+        const table = document.querySelector("table");
+        const rows = document.querySelectorAll('tr:not(:first-child)');
+        for (let row of rows) {
+            table.removeChild(row);
+        }
     }
 }
 
-function clearTable() {
-    const table = document.querySelector("table");
-    const rows = document.querySelectorAll('tr:not(:first-child)');
-    for (let row of rows) {
-        table.removeChild(row);
-    }
-}
+Book.addBookToLibrary("Haha", "Mem", 300, true);
+Book.addBookToLibrary("Hihi", "Hoho", 400, false);
 
-addBookToLibrary("Haha", "Mem", 300, true);
-addBookToLibrary("Hihi", "Hoho", 400, false);
-
-displayBooks()
+Book.displayBooks()
 
 const addButton = document.querySelector("button.add")
 const dialog = document.querySelector("dialog")
@@ -109,7 +109,7 @@ submitButton.addEventListener("click", function(event) {
     const pages = document.querySelector("#pages");
     const read = document.querySelector("#read");
 
-    addBookToLibrary(name.value, author.value, pages.value, read.checked);
+    Book.addBookToLibrary(name.value, author.value, pages.value, read.checked);
 
     name.value = "";
     author.value = "";
@@ -118,8 +118,8 @@ submitButton.addEventListener("click", function(event) {
 
     dialog.close();
     
-    clearTable();
-    displayBooks();
+    Book.clearTable();
+    Book.displayBooks();
 })
 
 const removeButtons = document.querySelectorAll(".remove");
